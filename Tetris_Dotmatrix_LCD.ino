26179eda0f45cad5768e31e37bff391bed8f9ede
Han ji wan <42163754+aufmcl@users.noreply.github.com>
2020-02-18 23:29:57 +0900

    Update Tetris_Dotmatrix_LCD.ino
    The block is shifted downward. But it doesn't stop on the floor.
    Create background and block arrays.
    Remove num_all array

--- Tetris_Dotmatrix_LCD.ino
+++ Tetris_Dotmatrix_LCD.ino
@@ -5,10 +5,10 @@
 
 void setup() {
   int i;
-  TCCR1A = 0x00;
-  TCCR1B = 0x04;
-  TIMSK1 = 0x01;
-  TCNT1 = 100;
+  //  TCCR1A = 0x00;
+  //  TCCR1B = 0x04;
+  //  TIMSK1 = 0x01;
+  //  TCNT1 = 100;
 
   for (i = 0; i < 16; i++) {
     pinMode(i + 2, OUTPUT);
@@ -21,19 +21,22 @@ void setup() {
       digitalWrite(i + 2, LOW);
     }
   }
+
+  Serial.begin(9600);
 }
 
-int num_all[10][8] = {
-  {0x00, 0x38, 0x44, 0x4c, 0x54, 0x64, 0x44, 0x38},
-  {0x00, 0x10, 0x30, 0x50, 0x10, 0x10, 0x10, 0x7c},
-  {0x00, 0x38, 0x44, 0x04, 0x08, 0x10, 0x20, 0x7c},
-  {0x00, 0x38, 0x44, 0x04, 0x18, 0x04, 0x44, 0x38},
-  {0x00, 0x08, 0x18, 0x28, 0x48, 0x7c, 0x08, 0x08},
-  {0x00, 0x7c, 0x40, 0x78, 0x04, 0x04, 0x44, 0x38},
-  {0x00, 0x38, 0x40, 0x40, 0x78, 0x44, 0x44, 0x38},
-  {0x00, 0x7c, 0x04, 0x08, 0x10, 0x20, 0x20, 0x20},
-  {0x00, 0x38, 0x44, 0x44, 0x38, 0x44, 0x44, 0x38},
-  {0x00, 0x38, 0x44, 0x44, 0x3c, 0x04, 0x44, 0x38}
+int block[8] = {0x10, 0x1C, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+int background[10][10] = {
+  {1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+  {1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+  {1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+  {1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+  {1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+  {1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+  {1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+  {1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+  {1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+  {1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
 };
 unsigned long c_micros = 0;
 unsigned long p_micros = 0;
@@ -42,69 +45,51 @@ unsigned long p_millis = 0;
 
 int count = 0;
 
-ISR(TIMER1_OVF_vect)
-{
-  TCNT1 = 100;
-  for (int j = 0; j < 8; j++) {
-    digitalWrite(j + 10, HIGH);
-    for (int i = 0; i < 8; i++) {
-      if (num_all[count][j] & (0x80 >> i)) {
-        digitalWrite(i + 2 , LOW);
-      }
-      else {
-        digitalWrite(i + 2 , HIGH);
-      }
-    }
-
-    //---------------------------OFF ALL
-    for (int i = 0; i < 18; i++) {
-      digitalWrite(i + 2, HIGH);
-
-      if (i >= 8) {
-        digitalWrite(i + 2, LOW);
-      }
-    }
-    //---------------------------
-  }
-}
+//ISR(TIMER1_OVF_vect)
+//{
+//  TCNT1 = 100;
+//}
 
 void loop() {
-  int i, j = 0;
   c_micros = micros();
   c_millis = millis();
 
-//  if (c_micros - p_micros > 2500) {
-//    p_micros = c_micros;
-//
-//    for (int j = 0; j < 8; j++) {
-//      digitalWrite(j + 10, HIGH);
-//      for (int i = 0; i < 8; i++) {
-//        if (num_all[count][j] & (0x80 >> i)) {
-//          digitalWrite(i + 2 , LOW);
-//        }
-//        else {
-//          digitalWrite(i + 2 , HIGH);
-//        }
-//      }
-//
-//      //---------------------------OFF ALL
-//      for (int i = 0; i < 18; i++) {
-//        digitalWrite(i + 2, HIGH);
-//
-//        if (i >= 8) {
-//          digitalWrite(i + 2, LOW);
-//        }
-//      }
-//      //---------------------------
-//    }
-//  }
+  if (c_micros - p_micros > 2500) {
+    p_micros = c_micros;
+
+    for (int j = 0; j < 8; j++) {
+      digitalWrite(j + 10, HIGH);
+      for (int i = 0; i < 8; i++) {
+        if (block[j] & (0x80 >> i)) {
+          digitalWrite(i + 2 , LOW);
+        }
+        else {
+          digitalWrite(i + 2 , HIGH);
+        }
+      }
+
+      //---------------------------OFF ALL
+      for (int i = 0; i < 18; i++) {
+        digitalWrite(i + 2, HIGH);
+
+        if (i >= 8) {
+          digitalWrite(i + 2, LOW);
+        }
+      }
+      //---------------------------
+    }
+  }
 
 
   if (c_millis - p_millis >= 1000) {
     p_millis = c_millis;
 
-    count++;
-    if (count >= 10)  count = 0;
+    //---------------------------DOWN SHIFT
+    for (int i = 7; i >= 0; i--) {
+      block[i] = block[i - 1];
+    }
+    block[0] = 0x00;
+    //---------------------------
   }
 
 }
