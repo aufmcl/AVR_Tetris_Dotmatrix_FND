a3a3caab873789706c424e65bc07cf890b1eb1f0
Han ji wan <42163754+aufmcl@users.noreply.github.com>
2020-02-23 22:37:58 +0900

    Update Tetris_Dotmatrix_LCD.ino
    Implement the create_block function and insert_block function.
    Create a new block, it doesn't shift down.

--- Tetris_Dotmatrix_LCD.ino
+++ Tetris_Dotmatrix_LCD.ino
@@ -2,6 +2,16 @@
 //2~9 column, 10~17 row
 //column LOW, row에 HIGH 일 때 ON
 
+int edge[8] = {0x81, 0x81, 0x81, 0x81, 0x81, 0x81, 0x81, 0xFF};
+int block[8] = {0x10, 0x1C, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+unsigned long c_micros = 0;
+unsigned long p_micros = 0;
+unsigned long c_millis = 0;
+unsigned long p_millis = 0;
+int falling_block[8] = {0x01, 0x1C, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+int fallen_block[8] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+int flag_crte = 1;
+
 
 void setup() {
   int i;
@@ -22,49 +32,80 @@ void setup() {
     }
   }
 
+  create_block();
+
   Serial.begin(9600);
 }
-
-
-int block[8] = {0x10, 0x1C, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
-int background[8] = {0x81, 0x81, 0x81, 0x81, 0x81, 0x81, 0x81, 0xFF};
-unsigned long c_micros = 0;
-unsigned long p_micros = 0;
-unsigned long c_millis = 0;
-unsigned long p_millis = 0;
 
 //ISR(TIMER1_OVF_vect)
 //{
 //  TCNT1 = 100;
 //}
 
-
 int overlap_check() {
-  for (int j = 0; j < 8; j++) {    
-    if (block[j] & background[j]) {
+
+  for (int i = 0; i < 8; i++) {
+    if (fallen_block[i] & edge[i]) {
       return 1;
-    }    
-  }  
+    }
+  }
   return 0;
+}
+
+void insert_block() {
+  for (int i = 0; i < 8; i++) {
+    fallen_block[i] |= falling_block[i];
+    edge[i] |= fallen_block[i];
+  }
+}
+
+void create_block() {
+  for (int i = 0; i < 8; i++) {
+    falling_block[i] = block[i];
+  }
 }
 
 void loop() {
   c_micros = micros();
   c_millis = millis();
 
+  if (c_millis - p_millis >= 1000) {
+    p_millis = c_millis;
+
+    if (overlap_check()) {
+      create_block();
+      insert_block();
+    }
+    else {
+      //---------------------------DOWN SHIFT
+      for (int i = 8; i >= 0; i--) {
+        falling_block[i] = falling_block[i - 1];
+      }
+      falling_block[0] = 0x00;
+      //---------------------------
+
+      for (int i = 0; i < 8; i++) {
+        fallen_block[i] = falling_block[i];
+      }
+    }
+  }
+
   if (c_micros - p_micros > 2500) {
     p_micros = c_micros;
 
     for (int j = 0; j < 8; j++) {
       digitalWrite(j + 10, HIGH);
+
+      //---------------------------DRAW BLOCK
       for (int i = 0; i < 8; i++) {
-        if (block[j] & (0x80 >> i)) {
+        if (fallen_block[j] & (0x80 >> i)) {
           digitalWrite(i + 2 , LOW);
         }
         else {
           digitalWrite(i + 2 , HIGH);
         }
       }
+      //---------------------------
 
       //---------------------------OFF ALL
       for (int i = 0; i < 18; i++) {
@@ -77,20 +118,4 @@ void loop() {
       //---------------------------
     }
   }
-
-
-  if (c_millis - p_millis >= 1000) {
-    p_millis = c_millis;
-
-    //---------------------------DOWN SHIFT
-    if (!overlap_check()) {
-      //memmove(block + 1, block, sizeof(int) * 8);
-      for (int i = 8; i >= 0; i--) {
-        block[i] = block[i - 1];
-      }
-      block[0] = 0x00;
-    }
-    //---------------------------
-  }
-
 }
