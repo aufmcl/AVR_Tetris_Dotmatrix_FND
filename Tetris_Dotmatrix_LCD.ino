5c926eb690a95ec9adf05829ca4bda21d220225e
Han ji wan <42163754+aufmcl@users.noreply.github.com>
2020-02-25 00:19:33 +0900

    Fix that did not come down after the new block was created
    The fallen block LED turns off when the overlap_check function returns 1.
    The overlap_check function returns 0 in the 0th index of the fallen_block array,
     but 1 in the 1st index. (Originally, the 0th index also returns 1)

--- Tetris_Dotmatrix_LCD.ino
+++ Tetris_Dotmatrix_LCD.ino
@@ -3,15 +3,38 @@
 //column LOW, row에 HIGH 일 때 ON
 
 int edge[8] = {0x81, 0x81, 0x81, 0x81, 0x81, 0x81, 0x81, 0xFF};
-int block[8] = {0x10, 0x1C, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+const int block[8] = {0x10, 0x1C, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+int falling_block[8] = {0x10, 0x1C, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+int fallen_block[8] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+int show_block[8];
 unsigned long c_micros = 0;
 unsigned long p_micros = 0;
 unsigned long c_millis = 0;
 unsigned long p_millis = 0;
-int falling_block[8] = {0x01, 0x1C, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
-int fallen_block[8] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
 int flag_crte = 1;
 
+
+int overlap_check() {
+  for (int i = 0; i < 8; i++) {
+    if (edge[i] & fallen_block[i]) {
+      return 1;
+    }
+  }
+  return 0;
+}
+
+void insert_block() {
+  for (int i = 0; i < 8; i++) {    
+    edge[i] |= fallen_block[i];
+    fallen_block[i] = falling_block[i];
+  }
+}
+
+void create_block() {
+  for (int i = 0; i < 8; i++) {
+    falling_block[i] = block[i];
+  }
+}
 
 void setup() {
   int i;
@@ -42,28 +65,7 @@ void setup() {
 //  TCNT1 = 100;
 //}
 
-int overlap_check() {
 
-  for (int i = 0; i < 8; i++) {
-    if (fallen_block[i] & edge[i]) {
-      return 1;
-    }
-  }
-  return 0;
-}
-
-void insert_block() {
-  for (int i = 0; i < 8; i++) {
-    fallen_block[i] |= falling_block[i];
-    edge[i] |= fallen_block[i];
-  }
-}
-
-void create_block() {
-  for (int i = 0; i < 8; i++) {
-    falling_block[i] = block[i];
-  }
-}
 
 void loop() {
   c_micros = micros();
@@ -72,11 +74,7 @@ void loop() {
   if (c_millis - p_millis >= 1000) {
     p_millis = c_millis;
 
-    if (overlap_check()) {
-      create_block();
-      insert_block();
-    }
-    else {
+    if (!overlap_check()) {
       //---------------------------DOWN SHIFT
       for (int i = 8; i >= 0; i--) {
         falling_block[i] = falling_block[i - 1];
@@ -87,6 +85,12 @@ void loop() {
       for (int i = 0; i < 8; i++) {
         fallen_block[i] = falling_block[i];
       }
+    }
+    else {
+      Serial.println("asdff");
+      create_block();
+      insert_block();
+      
     }
   }
 
